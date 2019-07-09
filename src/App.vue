d2404fb2722e45f715a731fbc1cb8e2321dde17f
hc <hc@gmail.com>
2019-07-09 14:21:29 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+  <div id="app">
+    <header class="Header">研报系统</header>
+    <router-view />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "App",
-}
+  name: "App"
+};
 </script>
+
+<style>
+.Header {
+  align-items: center;
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: #24292e;
+  color: hsla(0, 0%, 100%, 0.7);
+  font-size: 14px;
+  line-height: 30px;
+  padding: 16px;
+  z-index: 32;
+}
+</style>
+
