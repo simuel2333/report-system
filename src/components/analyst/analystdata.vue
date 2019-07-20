d2bc86580cf0521e668bb1e0f64a8c7508f354b5
simuel2333 <79045803@qq.com>
2019-07-20 19:05:51 +0800

    分页

--- src/components/analyst/analystdata.vue
+++ src/components/analyst/analystdata.vue
@@ -1,59 +1,104 @@
 <template>
-  <div class="container">
-    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
-      <el-table-column prop="no" label="股票代码"></el-table-column>
-      <el-table-column prop="name" label="股票名称"></el-table-column>
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column align="right">
-        <template slot="header">
-          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
-        </template>
-        <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+    <div class="container">
+        <el-table
+            :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase())).slice(start,end)"
+            style="margin:auto;width:90%"
+            :row-class-name="tableRowClassName"
+        >
+            <el-table-column prop="no" label="股票代码"></el-table-column>
+            <el-table-column prop="name" label="股票名称"></el-table-column>
+            <el-table-column prop="date" label="日期"></el-table-column>
+            <el-table-column align="center">
+                <template slot="header" slot-scope="scope">
+                    <el-input v-model="search" placeholder="输入股票名称搜索" clearable />
+                    {{scope.row}}
+                </template>
+                <template slot-scope="scope">
+                    <el-button
+                        size="mini"
+                        type="success"
+                        @click="handleEdit(scope.$index, scope.row)"
+                    >查看</el-button>
+                    <el-button
+                        size="mini"
+                        type="primary"
+                        @click="handleDelete(scope.$index, scope.row)"
+                    >运行</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+            style="text-align:right;width:90%;margin-top:20px"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            :page-size="pageSize"
+            layout="total, prev, pager, next, jumper"
+            :total="tableData.length"
+        ></el-pagination>
+    </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    tableRowClassName({ rowIndex }) {
-      if (rowIndex % 2 === 0) {
-        return "warning-row";
-      }
+    methods: {
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+        handleDelete(index, row) {
+            console.log(index, row);
+        },
+        tableRowClassName({ rowIndex }) {
+            if (rowIndex % 2 === 1) {
+                return "warning-row";
+            }
+            return "";
+        },
+        handleCurrentChange(val) {
+            this.currentPage = val;
+        }
     },
-    handleEdit(index, row) {
-      console.log(index, row);
+    data() {
+        const item1 = {
+            no: 601398,
+            name: "工商银行",
+            date: "2019.3.1~2019.9.30",
+            calc: "",
+            view: ""
+        };
+
+        const item2 = {
+            no: 600519,
+            name: "贵州茅台",
+            date: "2019.1.1~2019.6.30",
+            calc: "",
+            view: ""
+        };
+        let mockArray = Array(10).fill(item1);
+        mockArray = mockArray.concat(Array(10).fill(item2));
+        return {
+            tableData: mockArray,
+            search: "",
+            currentPage: 1,
+            pageSize: 10,
+        };
     },
-    handleDelete(index, row) {
-      console.log(index, row);
+    computed: {
+        start: function() {
+            return (this.currentPage - 1) * this.pageSize;
+        },
+        end: function() {
+            return this.start + this.pageSize;
+        }
     }
-  },
-  data() {
-    const item = {
-      no: 600519,
-      name: "贵州茅台",
-      date: "2019.1.1~2019.6.30",
-      calc: "",
-      view: ""
-    };
-    return {
-      tableData: Array(10).fill(item),
-      search: ""
-    };
-  }
 };
 </script>
 
 <style>
-.el-table .warning-row {
-  background: oldlace;
+.container {
+    width: 100%;
 }
-
-.el-table .success-row {
-  background: #f0f9eb;
+.el-table .warning-row {
+    background: oldlace;
+    text-align: center;
 }
 </style>
--- src/components/analyst/analystmain.vue
+++ src/components/analyst/analystmain.vue
@@ -1,74 +1,78 @@
 <template>
-  <el-container style="border: 1px solid #eee;height:calc(85vh);margin-top: 30px;">
-    <el-aside width="284px" style="background-color: rgb(238, 241, 246)">
-      <el-menu>
-        <el-menu-item index="1">
-          <i class="el-icon-s-data"></i>
-          <span slot="title">查看数据</span>
-        </el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-document"></i>查看报告
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-medal"></i>评级
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-orange"></i>情感分析
-          </template>
-        </el-submenu>
-        <el-submenu index="5">
-          <template slot="title">
-            <i class="el-icon-setting"></i>自动生成
-          </template>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-    <el-container>
-        <router-view></router-view>
+    <el-container style="border: 1px solid #eee;height:calc(85vh);margin-top: 30px;">
+        <el-aside width="284px" style="background-color: rgb(238, 241, 246)">
+            <el-menu>
+                <el-menu-item index="1">
+                    <i class="el-icon-s-data"></i>
+                    <span slot="title" @click="viewData()">查看数据</span>
+                </el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-document"></i>查看报告
+                    </template>
+                    <el-menu-item-group>
+                        <template slot="title">分组一</template>
+                        <el-menu-item index="2-1">选项1</el-menu-item>
+                        <el-menu-item index="2-2">选项2</el-menu-item>
+                    </el-menu-item-group>
+                    <el-menu-item-group title="分组2">
+                        <el-menu-item index="2-3">选项3</el-menu-item>
+                    </el-menu-item-group>
+                    <el-submenu index="2-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+                    </el-submenu>
+                </el-submenu>
+                <el-submenu index="3">
+                    <template slot="title">
+                        <i class="el-icon-medal"></i>评级
+                    </template>
+                    <el-menu-item-group>
+                        <template slot="title">分组一</template>
+                        <el-menu-item index="3-1">选项1</el-menu-item>
+                        <el-menu-item index="3-2">选项2</el-menu-item>
+                    </el-menu-item-group>
+                    <el-menu-item-group title="分组2">
+                        <el-menu-item index="3-3">选项3</el-menu-item>
+                    </el-menu-item-group>
+                    <el-submenu index="3-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+                    </el-submenu>
+                </el-submenu>
+                <el-submenu index="4">
+                    <template slot="title">
+                        <i class="el-icon-orange"></i>情感分析
+                    </template>
+                </el-submenu>
+                <el-submenu index="5">
+                    <template slot="title">
+                        <i class="el-icon-setting"></i>自动生成
+                    </template>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <router-view></router-view>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-    };
-  }
+    data() {
+        return {};
+    },
+    methods: {
+        viewData() {
+            this.$router.push("/analyst/data")
+        }
+    }
 };
 </script>
 
 <style scoped>
 .el-aside {
-  color: #333;
+    color: #333;
 }
 </style>
